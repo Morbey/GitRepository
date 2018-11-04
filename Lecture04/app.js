31f7a87f8f2cb780b8a2500de20f972b223d5996
Morbey <eduardomorbey@gmail.com>
2018-11-04 23:42:29 +0000

    Lecture04 finished

---
+++ Lecture04/app.js
@@ -0,0 +1,11 @@
+//IIFE Immediately Invoked Function Expression
+//Bind this app.js file to the index.html where it has the ng-app="myFirstApp"
+(function () {
+  'use strict'; //Avoids the declaration of global variables if they are not instantiated in the class
+  angular.module('myFirstApp', [])
+
+  //Controller bind to index.html div where it has ng-controller="MyFirstController"
+  .controller('MyFirstController', function() {
+
+  });
+})();
