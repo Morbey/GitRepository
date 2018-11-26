d7ba505a74a3aaa1ef9b2347525a1bc0c02aae4d
Morbey <eduardomorbey@gmail.com>
2018-11-26 22:56:15 +0000

    Lecture12

--- Lecture11/app.js
+++ Lecture11/app.js
@@ -7,9 +7,14 @@
   MsgController.$inject = ['$scope'];
   function MsgController ($scope) {
     $scope.name = "Eduardo";
+    $scope.stateOfBeing = "hungry";
 
     $scope.sayMessage = function() {
       return "Eduardo likes to eath healthy snacks at night! :)"
+    }
+
+    $scope.feedEduardo = function() {
+      $scope.stateOfBeing = "fed";
     }
   }
 })();
