14816ed619b853146d282ec49a5c13f6dbe2740c
Morbey <eduardomorbey@gmail.com>
2018-11-26 22:46:17 +0000

    Lecture11

--- Lecture11/app.js
+++ Lecture11/app.js
@@ -1,19 +1,15 @@
-// (function () {
-//   'use strict';
-//
-//   angular.module('DIApp', [])
-//   .controller('DIController', DIController);
-//
-//   DIController.$inject = ['$scope', '$filter'];
-//   function DIController ($scope, $filter) {
-//     $scope.name = "Eduardo";
-//
-//     $scope.upper = function () {
-//       var upCase = $filter('uppercase');
-//       $scope.name = upCase($scope.name);
-//     };
-//   }
-//
-//   console.log(DIController.toString());
-// })();
-!function(){"use strict";function n(n,e){n.name="Eduardo",n.upper=function(){var o=e("uppercase");n.name=o(n.name)}}angular.module("DIApp",[]).controller("DIController",n),n.$inject=["$scope","$filter"],console.log(n.toString())}();
+(function () {
+  'use strict';
+
+  angular.module('MsgApp', [])
+  .controller('MsgController', MsgController);
+
+  MsgController.$inject = ['$scope'];
+  function MsgController ($scope) {
+    $scope.name = "Eduardo";
+
+    $scope.sayMessage = function() {
+      return "Eduardo likes to eath healthy snacks at night! :)"
+    }
+  }
+})();
