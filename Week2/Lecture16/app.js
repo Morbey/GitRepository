31e45666db1431129f45905deff9524b46eb1a8a
Morbey <eduardomorbey@gmail.com>
2018-11-30 00:07:40 +0000

    Lecture16

--- Week2/Lecture16/app.js
+++ Week2/Lecture16/app.js
@@ -6,7 +6,7 @@ angular.module('BindingApp', [])
 
 BindingController.$inject = ['$scope'];
 function BindingController($scope) {
-  $scope.firstName = "Yaakov";
+  $scope.firstName = "Eduardo";
 
   $scope.showNumberOfWatchers = function () {
     console.log("# of Watchers: ", $scope.$$watchersCount);
